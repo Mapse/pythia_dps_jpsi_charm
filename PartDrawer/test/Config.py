1980ca8dfa421eef1b4437c24356a4661fd1437c
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2025-07-23 21:38:06 +0200

    all setup for particle drawer ready

--- PartDrawer/test/Config.py
+++ PartDrawer/test/Config.py
@@ -5,80 +5,107 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 process = cms.Process("GENLevelDump")
 import FWCore.Utilities.FileUtils as FileUtils
 
+## Parsing arguments
+
 options = VarParsing('analysis')
 
+# Number of events
+options.register ('nevents',
+				  '',
+				  VarParsing.multiplicity.singleton,
+				  VarParsing.varType.string,
+				  "number of events")
+
+# Particle channel
 options.register ('channel',
 				  '',
 				  VarParsing.multiplicity.singleton,
 				  VarParsing.varType.string,
 				  "Channel Chosen")
 
+# Energy (13TeV or 13p6TeV)
+options.register ('energy',
+				  '',
+				  VarParsing.multiplicity.singleton,
+				  VarParsing.varType.string,
+				  "Energy Chosen")
+
 options.parseArguments()
 
-## 2017 geometry 
-#from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+# Number of events
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(int(options.nevents)) )
 
-## Phase 2
-#from Configuration.Eras.Era_Phase2C8_timing_layer_bar_cff import Phase2C8_timing_layer_bar
-#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+## Choose the right channel as well as the energy to draw the particle list.
 
-## Global tag for 10.6 phase2 mc
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, '102X_mc2017_realistic_v8', '')
+channel = options.channel
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+# Jpsi + D0
+if channel == 'Jpsi_D0':
+  # 13 TeV
+  if options.energy == '13TeV':
+    path = 'jpsi_d0/files_path_jpsi_d0_13TeV.txt'
+    print('-------------- Particle List for Jpsi + D0 fragment at 13 TeV --------------')
+  # 13.6 TeV
+  elif options.energy == '13TeV':
+    path = 'jpsi_d0/files_path_jpsi_d0_13p6TeV.txt'
+    print('-------------- Particle List for Jpsi + D0 fragment at 13.6 TeV --------------')
+  else:
+    path = ''
+    print("Please, choose the available energies: 13TeV and 13p6TeV")
 
-# Choose the right channel to draw the particle list.
+# Jpsi + D+
+elif channel == 'Jpsi_Dplus':
+  # 13 TeV
+  if options.energy == '13TeV':
+    path = 'jpsi_dplus/files_path_jpsi_dplus_13TeV.txt'
+    print('-------------- Particle List for Jpsi + Dplus fragment at 13 TeV --------------')
+  # 13.6 TeV
+  elif options.energy == '13TeV':
+    path = 'jpsi_dplus/files_path_jpsi_dplus_13p6TeV.txt'
+    print('-------------- Particle List for Jpsi + Dplus fragment at 13.6 TeV --------------')
+  else:
+    path = ''
+    print("Please, choose the available energies: 13TeV and 13p6TeV")
 
-if options.channel == 'Jpsi_D0':
-  path = 'jpsi_d0/files_path_jpsi_d0.txt'
-  print('-------------- Particle List for Jpsi + D0 fragment at 13 TeV --------------')
-
-elif options.channel == 'Jpsi_Dplus':
-  path = 'jpsi_dplus/files_path_jpsi_dplus.txt'
-  print('-------------- Particle List for Jpsi + Dplus fragment at 13 TeV --------------')
-
-elif options.channel == 'Jpsi_Dsplus':
-  path = 'jpsi_dsplus/files_path_jpsi_dsplus.txt'
-  print('-------------- Particle List for Jpsi + Dsplus fragment at 13 TeV --------------')
+# Jpsi + Ds+
+elif channel == 'Jpsi_Dsplus':
+  # 13 TeV
+  if options.energy == '13TeV':
+    path = 'jpsi_dsplus/files_path_jpsi_dsplus_13TeV.txt'
+    print('-------------- Particle List for Jpsi + Dsplus fragment at 13 TeV --------------')
+  # 13.6 TeV
+  elif options.energy == '13TeV':
+    path = 'jpsi_dsplus/files_path_jpsi_dsplus_13p6TeV.txt'
+    print('-------------- Particle List for Jpsi + Dsplus fragment at 13.6 TeV --------------')
+  else:
+    path = ''
+    print("Please, choose the available energies: 13TeV and 13p6TeV")
 
 else:
-  print("wrong")
+  channel = ''
+  print("Please, choose the available channels: Jpsi_D0, Jpsi_Dplus, Jpsi_Dsplus")
 
 # File source
-mylist = FileUtils.loadListFromFile(path) 
-process.source = cms.Source(
-    "PoolSource",
-    #fileNames  = cms.untracked.vstring(options.inputFiles),
-    fileNames  = cms.untracked.vstring(*mylist),
-    duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+if path != '' and  channel != '':
+  mylist = FileUtils.loadListFromFile(path) 
+  process.source = cms.Source(
+      "PoolSource",
+      #fileNames  = cms.untracked.vstring(options.inputFiles),
+      fileNames  = cms.untracked.vstring(*mylist),
+      duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+  )
+
+
+  ## Show GenParticles
+  process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+  process.printTree = cms.EDAnalyzer("ParticleListDrawer",
+    maxEventsToPrint = cms.untracked.int32(10),
+    printVertex = cms.untracked.bool(True),
+    printOnlyHardInteraction = cms.untracked.bool(False), # Print only status=3 particles. This will not work for Pythia8, which does not have any such particles.
+    src = cms.InputTag("genParticles"), #genParticles
+      
 )
 
-
-## Show GenParticles
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
-process.printTree = cms.EDAnalyzer("ParticleListDrawer",
-  maxEventsToPrint = cms.untracked.int32(10),
-  printVertex = cms.untracked.bool(True),
-  printOnlyHardInteraction = cms.untracked.bool(False), # Print only status=3 particles. This will not work for Pythia8, which does not have any such particles.
-  src = cms.InputTag("genParticles"), #genParticles
-     
-)
-
-
-#process.genlevel = cms.EDAnalyzer('GenLevelStudies',
-# src = cms.InputTag("genParticles")
-#)
-
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2023_realistic_v2', '')
-
-#process.TFileService = cms.Service("TFileService",
-#	fileName = cms.string("mc_dump.root"),#
-#	closeFileFast = cms.untracked.bool(True))
-
-# Path and endpath to run the producer and output modules
-process.p =  cms.Path(process.printTree) #process.genlevel+
+  # Path and endpath to run the producer and output modules
+  process.p =  cms.Path(process.printTree) #process.genlevel+
 
